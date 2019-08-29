da1e06250c414bb658a1b763dfa2190e0ca11e40
relexers <mr.dmitrij.kiselev@gmail.com>
2019-08-29 21:08:52 +0300

    минимальная рабочая версия доски объявлений

--- board/newboard/forms.py
+++ board/newboard/forms.py
@@ -5,4 +5,4 @@ from .models import Bb
 class BbForm(ModelForm):
     class Meta:
         model = Bb
-        fields = ('title', 'content', 'price', 'rubric')
+        fields = ['title', 'content', 'price', 'rubric']
--- board/newboard/views.py
+++ board/newboard/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from .models import Bb, Rubric
 from django.views.generic.edit import CreateView
 from .forms import BbForm
-
+from django.urls import reverse_lazy
 
 def index(request):
     #template = loader.get_template('newboard/index.html')
@@ -25,7 +25,7 @@ def by_rubric(request, rubric_id):
 class BbCreateView(CreateView):
     template_name = 'newboard/create.html'
     form_class = BbForm
-    success_url = '/newboard/'
+    success_url = reverse_lazy('index')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
